35e1e8e1c9638abb222af0dba9b03a64021dffec
Thorsten Fischer <thorsten.fischer@zalando.de>
2012-09-17 11:17:07 +0200

    Added caching settings to solo.rb.
    Signed-off-by: Thorsten Fischer <thorsten.fischer@zalando.de>

--- solo.rb
+++ solo.rb
@@ -1,2 +1,4 @@
-file_cache_path "/home/thorsten/.chef/solo"
+file_cache_path "/home/thorsten/.chef/solo/cache"
 cookbook_path ["/home/thorsten/.chef/solo/cookbooks"]
+
+cache_options({ :path => "/home/thorsten/.chef/solo/cache", :skip_expires => true })
