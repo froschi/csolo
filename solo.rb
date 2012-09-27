a73ed111290eda595e924207ca6531352fac5908
Thorsten Fischer <thorsten@froschi.org>
2012-09-27 09:22:09 +0200

    Set cache_type to "Memory" in solo.rb.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 file_cache_path "/home/thorsten/.chef/solo/cache"
 file_backup_path "/home/thorsten/.chef/solo/cache/backup"
 cookbook_path ["/home/thorsten/.chef/solo/cookbooks"]
-
 cache_options({ :path => "/home/thorsten/.chef/solo/cache", :skip_expires => true })
+cache_type "Memory"
